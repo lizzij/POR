c6ab5b20e92ac021feeadcf5b881cfa975e03d45
Liz <elizazijinhuang@gmail.com>
2018-12-14 18:22:13 -0500

    add database

--- code/website/database.sql
+++ code/website/database.sql
@@ -9,3 +9,11 @@ DELETE FROM test WHERE wechatName='person1';
 DELETE FROM test WHERE wechatName='person2';
 ALTER TABLE test
     -> ADD COLUMN input VARCHAR(15) AFTER study_id;
+UPDATE mysql.user SET user='username' WHERE User = 'root';
+UPDATE mysql.user SET authentication_string = password('password') WHERE User = 'root';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'powerofrep';
+USE test;
+GRANT SUPER ON *.* TO root@localhost
+FLUSH PRIVILEGES;
+INSERT INTO mysql.user (Host, User, Password) VALUES ('%', 'root', password('powerofrep'));
+GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
