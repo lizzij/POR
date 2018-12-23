e20fbebb3d2b3e34eaf47d897910c123dd466ccf
Liz <elizazijinhuang@gmail.com>
2018-12-23 15:39:00 -0500

    finish buttons

--- website/scripts/survey.js
+++ website/scripts/survey.js
@@ -1,5 +1,13 @@
-function show(shown, hidden) {
-  document.getElementById(shown).style.display='block';
-  document.getElementById(hidden).style.display='none';
-  return false;
+function show(shown) {
+  var pages = ['page1', 'page2', 'page3', 'page4', 'page5'];
+  var pageIndex;
+  for (pageIndex = 0; pageIndex < pages.length; pageIndex++) {
+    pageId = pages[pageIndex];
+    if (shown == pageId) {
+      document.getElementById(shown).style.display='block';
+    }
+    else {
+      document.getElementById(pageId).style.display='none';
+    }
+  }
 }
