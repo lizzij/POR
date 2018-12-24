6b7a31a196df164378dc09009ba3b3e7401e26b3
Liz <elizazijinhuang@gmail.com>
2018-12-23 19:34:39 -0500

    slider highlight

--- website/scripts/survey.js
+++ website/scripts/survey.js
@@ -15,26 +15,34 @@ function show(shown) {
 function stateTrustSlide() {
   document.getElementById("stateAmount").value=document.getElementById("state").value
   var trust=document.getElementById("stateAmount").value;
-  var trustIds = ['trustScaleLabel1', 'pagtrustScaleLabel2', 'trustScaleLabel3',
+  var trustIds = ['trustScaleLabel1', 'trustScaleLabel2', 'trustScaleLabel3',
   'trustScaleLabel4', 'trustScaleLabel5'];
+  var selected;
+  for (i = 0; i < trustIds.length; i++) {
+    notSelected = trustIds[i];
+    document.getElementById(notSelected).style.color = "#979797";
+    document.getElementById(notSelected).style.fontWeight = "normal";
+
+  }
   if (0 <= trust && trust < 20) {
-    document.getElementById("trustScaleLabel1").innerHTML=document.getElementById("trustScaleLabel1").innerHTML.bold();
+    document.getElementById("trustScaleLabel1").innerHTML = document.getElementById("trustScaleLabel1").innerHTML.bold();
     document.getElementById("trustScaleLabel1").style.color = "#4F4F4F";
   }
   else if (20 <= trust && trust < 40) {
-    document.getElementById("trustScaleLabel2").innerHTML=document.getElementById("trustScaleLabel2").innerHTML.bold();
+    document.getElementById("trustScaleLabel2").innerHTML = document.getElementById("trustScaleLabel2").innerHTML.bold();
     document.getElementById("trustScaleLabel2").style.color = "#4F4F4F";
   }
   else if (40 <= trust && trust < 60) {
-    document.getElementById("trustScaleLabel3").innerHTML=document.getElementById("trustScaleLabel3").innerHTML.bold();
+    document.getElementById("trustScaleLabel3").innerHTML = document.getElementById("trustScaleLabel3").innerHTML.bold();
     document.getElementById("trustScaleLabel3").style.color = "#4F4F4F";
   }
   else if (60 <= trust && trust < 80) {
-    document.getElementById("trustScaleLabel4").innerHTML=document.getElementById("trustScaleLabel4").innerHTML.bold();
+    document.getElementById("trustScaleLabel4").innerHTML = document.getElementById("trustScaleLabel4").innerHTML.bold();
     document.getElementById("trustScaleLabel4").style.color = "#4F4F4F";
   }
   else if (80 <= trust && trust < 100) {
-    document.getElementById("trustScaleLabel5").innerHTML=document.getElementById("trustScaleLabel5").innerHTML.bold();
+    document.getElementById("trustScaleLabel5").innerHTML = document.getElementById("trustScaleLabel5").innerHTML.bold();
     document.getElementById("trustScaleLabel5").style.color = "#4F4F4F";
   }
+
 }
