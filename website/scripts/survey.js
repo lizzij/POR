63dfb720cb681cce5a928e2c9bb5c2cdb647ec6c
Liz <elizazijinhuang@gmail.com>
2018-12-25 02:27:58 -0500

    finish modification

--- website/scripts/survey.js
+++ website/scripts/survey.js
@@ -1,4 +1,5 @@
 function show(shown) {
+  document.getElementById("clearAllStarsButton").style.display='none';
   var pages = ['page1', 'page2', 'page3', 'page4', 'page5'];
   var pageIndex;
   for (pageIndex = 0; pageIndex < pages.length; pageIndex++) {
@@ -9,11 +10,13 @@ function show(shown) {
         var numStarLeft = document.getElementById("starLeftCount").innerHTML;
         console.log(numStarLeft);
         if ((parseInt(numStarLeft)) > 0) {
-          document.getElementById("starAlert").innerHTML = '请用完所有星星!';
+          document.getElementById("starAlert").innerHTML = '请用完所有星星！';
           document.getElementById('page4').style.display='block';
+          document.getElementById("clearAllStarsButton").style.display='block';
         }
         else if (alert != '') {
           document.getElementById('page4').style.display='block';
+          document.getElementById("clearAllStarsButton").style.display='block';
         }
         else {
           document.getElementById('page5').style.display='block';
@@ -150,16 +153,23 @@ function startTimer() {
 }
 
 function starCountGroup1(number) {
+  var starLeftCount = document.getElementById("starLeftCount").innerHTML;
   var count = document.getElementById("starCountGroup1").innerHTML;
-  if (number == 1 && count == 0) {
+  if (number - count > starLeftCount) {
+    document.getElementById("starAlert").innerHTML = '超过12个星星！';
+  }
+  else if (number == 1 && count == 0) {
+    document.getElementById("starAlert").innerHTML = '';
     document.getElementById("starGroup1Cover").innerHTML = '<div class="star" onclick="starCountGroup1(1)">&starf;</div>';
     document.getElementById("starCountGroup1").innerHTML = '1';
   }
   else if (number == 1 && count == 1) {
+    document.getElementById("starAlert").innerHTML = '';
     document.getElementById("starGroup1Cover").innerHTML = '';
     document.getElementById("starCountGroup1").innerHTML = '0';
   }
   else {
+    document.getElementById("starAlert").innerHTML = '';
     var star = '';
     var i;
     for (i = 0; i < number; i++) {
@@ -168,21 +178,30 @@ function starCountGroup1(number) {
     document.getElementById("starGroup1Cover").innerHTML = star;
     document.getElementById("starCountGroup1").innerHTML = number;
   }
+  document.getElementById("groupDescription").innerHTML =
+  "优：空气质量令人满意，基本无空气污染。各类人群可正常活动。";
   starLeft();
   return false;
 }
 
 function starCountGroup2(number) {
+  var starLeftCount = document.getElementById("starLeftCount").innerHTML;
   var count = document.getElementById("starCountGroup2").innerHTML;
-  if (number == 1 && count == 0) {
+  if (number - count > starLeftCount) {
+    document.getElementById("starAlert").innerHTML = '超过12个星星！';
+  }
+  else if (number == 1 && count == 0) {
+    document.getElementById("starAlert").innerHTML = '';
     document.getElementById("starGroup2Cover").innerHTML = '<div class="star" onclick="starCountGroup2(1)">&starf;</div>';
     document.getElementById("starCountGroup2").innerHTML = '1';
   }
   else if (number == 1 && count == 1) {
+    document.getElementById("starAlert").innerHTML = '';
     document.getElementById("starGroup2Cover").innerHTML = '';
     document.getElementById("starCountGroup2").innerHTML = '0';
   }
   else {
+    document.getElementById("starAlert").innerHTML = '';
     var star = '';
     var i;
     for (i = 0; i < number; i++) {
@@ -192,20 +211,29 @@ function starCountGroup2(number) {
     document.getElementById("starCountGroup2").innerHTML = number;
   }
   starLeft();
+  document.getElementById("groupDescription").innerHTML =
+  "良：空气质量可接受，但某些污染物可能对极少数异常敏感人群健康有较弱影响。极少数异常敏感人群应减少户外活动。";
   return false;
 }
 
 function starCountGroup3(number) {
+  var starLeftCount = document.getElementById("starLeftCount").innerHTML;
   var count = document.getElementById("starCountGroup3").innerHTML;
-  if (number == 1 && count == 0) {
+  if (number - count > starLeftCount) {
+    document.getElementById("starAlert").innerHTML = '超过12个星星！';
+  }
+  else if (number == 1 && count == 0) {
+    document.getElementById("starAlert").innerHTML = '';
     document.getElementById("starGroup3Cover").innerHTML = '<div class="star" onclick="starCountGroup3(1)">&starf;</div>';
     document.getElementById("starCountGroup3").innerHTML = '1';
   }
   else if (number == 1 && count == 1) {
+    document.getElementById("starAlert").innerHTML = '';
     document.getElementById("starGroup3Cover").innerHTML = '';
     document.getElementById("starCountGroup3").innerHTML = '0';
   }
   else {
+    document.getElementById("starAlert").innerHTML = '';
     var star = '';
     var i;
     for (i = 0; i < number; i++) {
@@ -215,20 +243,29 @@ function starCountGroup3(number) {
     document.getElementById("starCountGroup3").innerHTML = number;
   }
   starLeft();
+  document.getElementById("groupDescription").innerHTML =
+  "轻度污染：易感人群症状有轻度加剧，健康人群出现刺激症状。儿童、老年人及心脏病、呼吸系统疾病患者应减少长时间、高强度的户外锻炼。";
   return false;
 }
 
 function starCountGroup4(number) {
+  var starLeftCount = document.getElementById("starLeftCount").innerHTML;
   var count = document.getElementById("starCountGroup4").innerHTML;
-  if (number == 1 && count == 0) {
+  if (number - count > starLeftCount) {
+    document.getElementById("starAlert").innerHTML = '超过12个星星！';
+  }
+  else if (number == 1 && count == 0) {
+    document.getElementById("starAlert").innerHTML = '';
     document.getElementById("starGroup4Cover").innerHTML = '<div class="star" onclick="starCountGroup4(1)">&starf;</div>';
     document.getElementById("starCountGroup4").innerHTML = '1';
   }
   else if (number == 1 && count == 1) {
+    document.getElementById("starAlert").innerHTML = '';
     document.getElementById("starGroup4Cover").innerHTML = '';
     document.getElementById("starCountGroup4").innerHTML = '0';
   }
   else {
+    document.getElementById("starAlert").innerHTML = '';
     var star = '';
     var i;
     for (i = 0; i < number; i++) {
@@ -238,20 +275,29 @@ function starCountGroup4(number) {
     document.getElementById("starCountGroup4").innerHTML = number;
   }
   starLeft();
+  document.getElementById("groupDescription").innerHTML =
+  "中度污染：进一步加剧易感人群症状，可能对健康人群心脏、呼吸系统有影响。儿童、老年人及心脏病、呼吸系统疾病患者避免长时间、高强度的户外锻练，一般人群适量减少户外运动。";
   return false;
 }
 
 function starCountGroup5(number) {
+  var starLeftCount = document.getElementById("starLeftCount").innerHTML;
   var count = document.getElementById("starCountGroup5").innerHTML;
-  if (number == 1 && count == 0) {
+  if (number - count > starLeftCount) {
+    document.getElementById("starAlert").innerHTML = '超过12个星星！';
+  }
+  else if (number == 1 && count == 0) {
+    document.getElementById("starAlert").innerHTML = '';
     document.getElementById("starGroup5Cover").innerHTML = '<div class="star" onclick="starCountGroup5(1)">&starf;</div>';
     document.getElementById("starCountGroup5").innerHTML = '1';
   }
   else if (number == 1 && count == 1) {
+    document.getElementById("starAlert").innerHTML = '';
     document.getElementById("starGroup5Cover").innerHTML = '';
     document.getElementById("starCountGroup5").innerHTML = '0';
   }
   else {
+    document.getElementById("starAlert").innerHTML = '';
     var star = '';
     var i;
     for (i = 0; i < number; i++) {
@@ -261,20 +307,29 @@ function starCountGroup5(number) {
     document.getElementById("starCountGroup5").innerHTML = number;
   }
   starLeft();
+  document.getElementById("groupDescription").innerHTML =
+  "重度污染：心脏病和肺病患者症状显著加剧，运动耐受力降低，健康人群普遍出现症状。儿童、老年人和心脏病、肺病患者应停留在室内，停止户外运动，一般人群减少户外运动。";
   return false;
 }
 
 function starCountGroup6(number) {
+  var starLeftCount = document.getElementById("starLeftCount").innerHTML;
   var count = document.getElementById("starCountGroup6").innerHTML;
-  if (number == 1 && count == 0) {
+  if (number - count > starLeftCount) {
+    document.getElementById("starAlert").innerHTML = '超过12个星星！';
+  }
+  else if (number == 1 && count == 0) {
+    document.getElementById("starAlert").innerHTML = '';
     document.getElementById("starGroup6Cover").innerHTML = '<div class="star" onclick="starCountGroup6(1)">&starf;</div>';
     document.getElementById("starCountGroup6").innerHTML = '1';
   }
   else if (number == 1 && count == 1) {
+    document.getElementById("starAlert").innerHTML = '';
     document.getElementById("starGroup6Cover").innerHTML = '';
     document.getElementById("starCountGroup6").innerHTML = '0';
   }
   else {
+    document.getElementById("starAlert").innerHTML = '';
     var star = '';
     var i;
     for (i = 0; i < number; i++) {
@@ -284,11 +339,12 @@ function starCountGroup6(number) {
     document.getElementById("starCountGroup6").innerHTML = number;
   }
   starLeft();
+  document.getElementById("groupDescription").innerHTML =
+  "严重污染：健康人群运动耐受力降低，有明显强烈症状，提前出现某些疾病。儿童、老年人和病人应当留在室内，避免体力消耗，一般人群应避免户外活动。";
   return false;
 }
 
 function starLeft() {
-  document.getElementById("starAlert").innerHTML = '';
   var starLeftCount = 12;
   var starLeft = '';
   starLeftCount = starLeftCount
@@ -298,15 +354,42 @@ function starLeft() {
   - document.getElementById("starCountGroup4").innerHTML
   - document.getElementById("starCountGroup5").innerHTML
   - document.getElementById("starCountGroup6").innerHTML;
-  if (starLeftCount < 0) {
-    document.getElementById("starAlert").innerHTML = '超过12个星星！';
-  }
-  else {
+  // if (starLeftCount < 0) {
+  //   document.getElementById("starAlert").innerHTML = '超过12个星星！';
+  // }
+  // else {
     var i;
     for (i = 0; i < starLeftCount; i++) {
       starLeft = starLeft + '<div class="star">&starf;</div>';
     }
-  }
+  // }
   document.getElementById("starLeftCount").innerHTML = starLeftCount + "";
   document.getElementById("starLeftContainer").innerHTML = starLeft;
+  showClearAllStarsButton();
+}
+
+function showClearAllStarsButton() {
+  var starLeftCount = document.getElementById("starLeftCount").innerHTML;
+  if ((parseInt(starLeftCount)) < 12) {
+    document.getElementById("clearAllStarsButton").style.display='block';
+  }
+  else {
+    document.getElementById("clearAllStarsButton").style.display='none';
+  }
+}
+
+function clearAllStars() {
+  document.getElementById("starAlert").innerHTML = '';
+  var starCounts = ["starCountGroup1", "starCountGroup2", "starCountGroup3",
+  "starCountGroup4", "starCountGroup5", "starCountGroup6"];
+  var starCovers = ["starGroup1Cover", "starGroup2Cover", "starGroup3Cover",
+  "starGroup4Cover", "starGroup5Cover", "starGroup6Cover"];
+  var i;
+  for (i = 0; i < starCounts.length; i++) {
+    var starCount = starCounts[i];
+    document.getElementById(starCount).innerHTML = '0';
+    var starCover = starCovers[i];
+    document.getElementById(starCover).innerHTML = '';
+  }
+  starLeft();
 }
