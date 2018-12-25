20305ff6269704a85d8a8efbc3b2d5c38fd5637f
Liz <elizazijinhuang@gmail.com>
2018-12-24 22:16:45 -0500

    button moved up

--- website/database/database.sql
+++ website/database/database.sql
@@ -17,3 +17,15 @@ GRANT SUPER ON *.* TO root@localhost
 FLUSH PRIVILEGES;
 INSERT INTO mysql.user (Host, User, Password) VALUES ('%', 'root', password('powerofrep'));
 GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
+
+
+-- mysql
+ -- mysql -u root -p
+show databases;
+CREATE DATABASE surveyTest;
+USE surveyTest;
+CREATE TABLE test (hukou VARCHAR(20), trust_state VARCHAR(20), trust_private VARCHAR(20), trust_foreign VARCHAR(20), uni VARCHAR(20));
+SHOW TABLES;
+show index from test;
+SELECT * FROM test;
+INSERT INTO test ( hukou, trust_state, trust_private, trust_foreign, uni ) VALUES ( "urban", "1", "1", "1", "Fudan Uni");
