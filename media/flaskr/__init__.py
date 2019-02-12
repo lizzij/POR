741b658dc6ef314965543e5f47dd1d112fd14a6b
Liz <elizazijinhuang@gmail.com>
2019-02-12 06:00:30 -0500

    home and info page

---
+++ media/flaskr/__init__.py
@@ -0,0 +1,38 @@
+import os
+
+from flask import Flask, render_template
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # home page
+    @app.route('/')
+    @app.route('/home')
+    def home():
+        return render_template('home.html')
+
+    # information page
+    @app.route('/<int:user_id>/<int:day>/info')
+    def info(user_id, day):
+        return render_template('info.html', user_id=user_id, day=day)
+
+    return app
