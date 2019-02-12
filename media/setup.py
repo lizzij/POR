cb15219a8b1b866f1664bde77cca6e61e1a735ff
Liz <elizazijinhuang@gmail.com>
2019-02-12 09:33:51 -0500

    transition logic

---
+++ media/setup.py
@@ -0,0 +1,11 @@
+# from setuptools import setup
+#
+# setup(
+#     name='media',
+#     packages=['media'],
+#     include_package_data=True,
+#     install_requires=[
+#         'flask',
+#         'pycrypto',
+#     ]
+# )
