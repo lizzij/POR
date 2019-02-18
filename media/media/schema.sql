79d2752aa431e20388df5757a3a8cd94d611d94c
Liz <elizazijinhuang@gmail.com>
2019-02-17 23:41:55 -0500

    db eg

--- media/media/schema.sql
+++ media/media/schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS survey;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER NOT NULL,
+  user_id INTEGER UNIQUE NOT NULL,
   day INTEGER NOT NULL,
   wechat_id TEXT UNIQUE NOT NULL,
   treatment TEXT NOT NULL
@@ -11,8 +11,18 @@ CREATE TABLE user (
 
 CREATE TABLE survey (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER NOT NULL,
+  user_id INTEGER UNIQUE NOT NULL,
   result TEXT NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES user (user_id)
 );
+
+INSERT INTO user (user_id, day, wechat_id, treatment)
+VALUES
+  (1, 1, 'wechat_id1', 'treatment1'),
+  (2, 2, 'wechat_id2', 'treatment2'),
+  (3, 1, 'wechat_id3', 'treatment3');
+
+INSERT INTO survey (user_id, result, created)
+VALUES
+  (3, 'Fudan Uni', '2018-01-01 00:00:00');
--- media/tests/data.sql
+++ media/tests/data.sql
@@ -1,15 +1,9 @@
 INSERT INTO user (day, wechat_id, treatment)
 VALUES
-  ('1', '1','wechat_id1','treatment1'),
-  ('1', '2','wechat_id1','treatment1'),
-  ('1', '3','wechat_id1','treatment1'),
-  ('2', '1','wechat_id2','treatment2'),
-  ('2', '2','wechat_id2','treatment2'),
-  ('2', '3','wechat_id2','treatment2'),
-  ('3', '1','wechat_id3','treatment3'),
-  ('3', '2','wechat_id3','treatment3'),
-  ('3', '3','wechat_id3','treatment3');
+  (1, 1, 'wechat_id1', 'treatment1'),
+  (2, 2, 'wechat_id2', 'treatment2'),
+  (3, 1, 'wechat_id3', 'treatment3');
 
 INSERT INTO survey (survey_id, result, created)
 VALUES
-  ('3', 'Fudan Uni', '2018-01-01 00:00:00');
+  (3, 'Fudan Uni', '2018-01-01 00:00:00');
