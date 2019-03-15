0fb07d9cbde601d78b4982031b38af000b62a99e
eliza <elizazijinhuang@gmail.com>
2019-03-15 08:08:15 -0400

    working date hashids

--- media/media/utils.py
+++ media/media/utils.py
@@ -7,24 +7,32 @@ def current_time():
     now = unicode(now)
     return now
 
+user_id_hashids = Hashids()
 # hashing and decoding user_id of length 16
-user_id_hashids = Hashids("power of repetition" + current_time(), min_length=16)
+def update_user_id_hashids():
+    global user_id_hashids
+    user_id_hashids = Hashids("power of repetition" + current_time(), min_length=16)
 
 def create_user_id_hashid(id):
+    update_user_id_hashids()
     hashid = user_id_hashids.encrypt(id);
-    return current_time()
+    return hashid
 
 def decode_user_id_hashid(hashid):
     id = user_id_hashids.decrypt(hashid)
-    return current_time()
+    return id
 
+day_hashids = Hashids()
 # hashing and decoding day of length 10
-day_hashids = Hashids("jfolozgobmqdyynzxozu" + current_time(), min_length=10)
+def update_day_hashids():
+    global day_hashids
+    day_hashids = Hashids("jfolozgobmqdyynzxozu" + current_time(), min_length=10)
 
 def create_day_hashid(id):
+    update_day_hashids()
     hashid = day_hashids.encrypt(id);
-    return current_time()
+    return hashid
 
 def decode_day_hashid(hashid):
     id = day_hashids.decrypt(hashid)
-    return current_time()
+    return id
