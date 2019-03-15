da1e828549679561674c4d95f840fa96a7cfeeb2
eliza <elizazijinhuang@gmail.com>
2019-03-15 07:53:04 -0400

    hash by time

--- media/media/utils.py
+++ media/media/utils.py
@@ -1,24 +1,30 @@
 # -*- coding: utf-8 -*-
+import datetime
 from hashids import Hashids
 
+def current_time():
+    now = datetime.datetime.now()
+    now = unicode(now)
+    return now
+
 # hashing and decoding user_id of length 16
-user_id_hashids = Hashids("power of repetition", min_length=16)
+user_id_hashids = Hashids("power of repetition" + current_time(), min_length=16)
 
 def create_user_id_hashid(id):
     hashid = user_id_hashids.encrypt(id);
-    return hashid
+    return current_time()
 
 def decode_user_id_hashid(hashid):
     id = user_id_hashids.decrypt(hashid)
-    return id
+    return current_time()
 
 # hashing and decoding day of length 10
-day_hashids = Hashids("jfolozgobmqdyynzxozu", min_length=10)
+day_hashids = Hashids("jfolozgobmqdyynzxozu" + current_time(), min_length=10)
 
 def create_day_hashid(id):
     hashid = day_hashids.encrypt(id);
-    return hashid
+    return current_time()
 
 def decode_day_hashid(hashid):
     id = day_hashids.decrypt(hashid)
-    return id
+    return current_time()
