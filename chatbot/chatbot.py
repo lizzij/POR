b38991b489d2c6a7b99fd859f3033cac9792f9e1
eliza <elizazijinhuang@gmail.com>
2019-04-04 16:02:20 -0400

    chatbot setting

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -13,6 +13,8 @@ days = []
 wechat_ids = []
 treatments = []
 urls = []
+completes = []
+urls2 = []
 def get_participants_info():
     with open('test.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -26,8 +28,10 @@ def get_participants_info():
                 wechat_ids.append(row[2])
                 treatments.append(row[3])
                 urls.append(row[4])
+                completes.append(row[5])
+                urls2.append(row[6])
                 line_count += 1
-    return [user_ids, days, wechat_ids, treatments, urls]
+    return [user_ids, days, wechat_ids, treatments, urls, completes, urls2]
 
 # # add friend
 # def add_friends():
@@ -45,6 +49,9 @@ intro = u'  此次调研总共维持8天时间。\
 调研结束后我们将进行抽奖，所有参与并完成调研的同学将有机会赢得800元人民币作为奖励。\
 这是一条自动消息。'
 
+reminder = u'您没有完成昨天的调查。我们理解您可能有别的事在忙。 我们将再给您一整天的时间来完成昨天的调研。\
+如您所知，只有在完成所有8天的调研后，您才有机会参与赢得800元人民币的抽奖，并收到来自哈佛大学研究员的参与证明。 这里是链接！'
+
 # auto accept friend request
 @bot.register(msg_types=FRIENDS)
 def auto_accept_friends(msg):
@@ -55,12 +62,20 @@ if __name__ == "__main__":
     # add_friends()
 
     bot.enable_puid('wxpy_puid.pkl')
-    tester = bot.friends().search('test')[0]
+    tester = bot.friends().search('liz')[0]
     tester.send(intro)
 
     time.sleep(2)
     urls = get_participants_info()[4]
-    tester.send(urls[2])
+    tester.send(urls[0])
+
+    time.sleep(100) # change to a day later
+    completes = get_participants_info()[5]
+    if completes[0] == 'F':
+        tester.send(urls2[0])
+    else:
+        tester.send(reminder)
+        tester.send(urls[0])
 
 # keep login-ed
 embed()
