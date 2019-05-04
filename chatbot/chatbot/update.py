8c6349b0f7e2744fe0df1035d4c1f11d09454d38
eliza <elizazijinhuang@gmail.com>
2019-05-03 21:07:07 -0400

    results, events scrape

--- chatbot/chatbot/update.py
+++ chatbot/chatbot/update.py
@@ -3,4 +3,4 @@ import requests
 # requests.post("https://dailyeventinfo.com/userInsert/"+str(nextUserID)+"/"+
     # str(day)+"/"+str(userName)+"/"+"T1"+"/"+hashed_user_id+"/"+hashed_day)
 
-requests.post("https://dailyeventinfo.com/activityUpdate/"+str(8194629)+"/2/1/5/0/0")
+requests.post("http://127.0.0.1:5000/activityUpdate/"+str(1)+"/0/0/0/0/0")
