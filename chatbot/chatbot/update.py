f4db2da1a4f9e2f18d6475f14968f9409396a094
eliza <elizazijinhuang@gmail.com>
2019-05-06 10:11:14 -0400

    lastest results

--- chatbot/chatbot/update.py
+++ chatbot/chatbot/update.py
@@ -5,7 +5,7 @@ import requests
 
 # /activityUpdate/<user_id>/<day>/<day_complete>/<survey_page>/<h1>/<h2>
 requests.post("http://127.0.0.1:5000/activityUpdate/"+str(1)+"/1/0/0/0/0")
-requests.post("http://127.0.0.1:5000/activityUpdate/"+str(2)+"/2/0/0/0/0")
-requests.post("http://127.0.0.1:5000/activityUpdate/"+str(3)+"/6/0/0/0/0")
-requests.post("http://127.0.0.1:5000/activityUpdate/"+str(4)+"/7/0/0/0/0")
-requests.post("http://127.0.0.1:5000/activityUpdate/"+str(5)+"/8/0/0/0/0")
+requests.post("http://127.0.0.1:5000/activityUpdate/"+str(2)+"/1/0/0/0/0")
+requests.post("http://127.0.0.1:5000/activityUpdate/"+str(3)+"/1/0/0/0/0")
+requests.post("http://127.0.0.1:5000/activityUpdate/"+str(4)+"/1/0/0/0/0")
+requests.post("http://127.0.0.1:5000/activityUpdate/"+str(5)+"/1/0/0/0/0")
