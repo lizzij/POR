b0a8f56280103d16b210c225054a7cbc66f7b998
eliza <elizazijinhuang@gmail.com>
2019-05-09 06:37:53 -0400

    deleted user exception

--- chatbot/chatbot/update.py
+++ chatbot/chatbot/update.py
@@ -4,8 +4,8 @@ import requests
     # str(day)+"/"+str(userName)+"/"+"T1"+"/"+hashed_user_id+"/"+hashed_day)
 
 # /activityUpdate/<user_id>/<day>/<day_complete>/<survey_page>/<h1>/<h2>
-requests.post("http://127.0.0.1:5000/activityUpdate/"+str(1)+"/7/0/0/0/0")
-requests.post("http://127.0.0.1:5000/activityUpdate/"+str(2)+"/1/0/0/0/0")
-requests.post("http://127.0.0.1:5000/activityUpdate/"+str(3)+"/1/0/0/0/0")
-requests.post("http://127.0.0.1:5000/activityUpdate/"+str(4)+"/1/0/0/0/0")
-requests.post("http://127.0.0.1:5000/activityUpdate/"+str(5)+"/1/0/0/0/0")
+requests.post("http://127.0.0.1:5000/activityUpdate/"+str(1)+"/2/0/0/0/0")
+requests.post("http://127.0.0.1:5000/activityUpdate/"+str(2)+"/6/0/0/0/0")
+requests.post("http://127.0.0.1:5000/activityUpdate/"+str(3)+"/6/0/0/0/0")
+requests.post("http://127.0.0.1:5000/activityUpdate/"+str(4)+"/6/0/0/0/0")
+requests.post("http://127.0.0.1:5000/activityUpdate/"+str(5)+"/6/0/0/0/0")
