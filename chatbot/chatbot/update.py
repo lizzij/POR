eba64f4ff866b43dd8798872694757747bf017ea
eliza <elizazijinhuang@gmail.com>
2019-05-26 19:14:38 -0400

    day 7 data analysis

--- chatbot/chatbot/update.py
+++ chatbot/chatbot/update.py
@@ -4,10 +4,17 @@ import requests
     # str(day)+"/"+str(userName)+"/"+"T1"+"/"+hashed_user_id+"/"+hashed_day)
 
 # /activityUpdate/<user_id>/<day>/<day_complete>/<survey_page>/<h1>/<h2>
-requests.post("https://dailyeventinfo.com/activityUpdate/"+str(2)+"/6/0/0/0/0")
-requests.post("https://dailyeventinfo.com/activityUpdate/"+str(3)+"/7/0/0/0/0")
+
 # requests.post("http://127.0.0.1:5000/activityUpdate/"+str(1)+"/1/0/0/0/0")
 # requests.post("http://127.0.0.1:5000/activityUpdate/"+str(2)+"/3/0/0/0/0")
 # requests.post("http://127.0.0.1:5000/activityUpdate/"+str(3)+"/4/0/0/0/0")
 # requests.post("http://127.0.0.1:5000/activityUpdate/"+str(4)+"/6/0/0/0/0")
 # requests.post("http://127.0.0.1:5000/activityUpdate/"+str(5)+"/7/0/0/0/0")
+
+requests.post("https://dailyeventinfo.com/activityUpdate/"+str(14276185)+"/7/1/4/0/0")
+requests.post("https://dailyeventinfo.com/activityUpdate/"+str(15312225)+"/7/1/4/0/0")
+requests.post("https://dailyeventinfo.com/activityUpdate/"+str(16067600)+"/7/1/4/0/0")
+requests.post("https://dailyeventinfo.com/activityUpdate/"+str(17737049)+"/7/0/1/0/0")
+requests.post("https://dailyeventinfo.com/activityUpdate/"+str(18845429)+"/7/1/4/0/0")
+requests.post("https://dailyeventinfo.com/activityUpdate/"+str(21853588)+"/7/1/4/0/0")
+requests.post("https://dailyeventinfo.com/activityUpdate/"+str(22535066)+"/7/1/4/0/0")
