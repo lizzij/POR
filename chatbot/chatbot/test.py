c68d1328c29f3742962365fc142bcb3e8ecc4b1f
eliza <elizazijinhuang@gmail.com>
2019-05-30 10:00:37 -0400

    document wechat-werun method

--- chatbot/chatbot/test.py
+++ chatbot/chatbot/test.py
@@ -7,9 +7,8 @@ myself = bot.self
 bot.file_helper.send('(Placeholder for instructions to install WeRun)')
 
 bot.file_helper.send_raw_msg(
-# 名片的原始消息类型
 raw_type=42,
-# 注意 `username` 在这里应为微信 ID，且被发送的名片必须为自己的好友
+# bot must be friend with WeRun-WeChat
 raw_content='<msg username="WeRun-WeChat" nickname="微信运动"/>'
 )
 
