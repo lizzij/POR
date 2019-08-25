a40298b6af624ee8719e89873169a3d168aa0917
eliza <elizazijinhuang@gmail.com>
2019-08-25 13:27:11 -0400

    test for sh cohort

--- distribution/chatbot/xian/test_update_activity_day.py
+++ distribution/chatbot/xian/test_update_activity_day.py
@@ -2,8 +2,9 @@ import requests
 
 # this script updates users to 0/0/0/0 activity
 
+day_update_to = input('Which day (1-8) do you want the user to be updated to? ')
 input_id_lower = input('\nPlease input a test nextUserID lower bound (inclusive): ')
 input_id_upper = input('Please input a test nextUserID upper bound (exclusive): ')
 
 for nextUserID in range(int(input_id_lower), int(input_id_upper)):
-    requests.post("http://127.0.0.1:5000/activityUpdate/"+str(nextUserID)+ "/2/0/0/0/0")
+    requests.post("http://127.0.0.1:5000/activityUpdate/"+str(nextUserID)+"/"+str(day_update_to)+"/0/0/0/0")
