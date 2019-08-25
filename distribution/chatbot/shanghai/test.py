a285a0051db683698f50e4a4674156d8bb84e158
eliza <elizazijinhuang@gmail.com>
2019-08-25 14:06:53 -0400

    fix test for 4 treatment, user-input day

--- distribution/chatbot/shanghai/test.py
+++ distribution/chatbot/shanghai/test.py
@@ -5,17 +5,13 @@ import pandas as pd
 import numpy as np
 from tabulate import tabulate
 
-treat_no = ['T1', 'T2', 'T3', 'T4']
-treat_prob = [0.25, 0.25, 0.25, 0.25]
-
-input_id_lower = input('\nPlease input a test nextUserID lower bound (inclusive): ')
-input_id_upper = input('Please input a test nextUserID upper bound (exclusive): ')
-
-lower = int(input_id_lower)
-upper = int(input_id_upper)
+day = int(input('\nWhich day (0-8) do you want to test? '))
+lower = 1000
+upper = 1004
+treatments = ['T1', 'T2', 'T3', 'T4']
 dict = {
-    'nextUserID': np.repeat(np.arange(lower, upper, 1), 2),
-    'day': np.tile(np.arange([9]), (upper-lower)), # day 0 (consent) - day 8 (inclusive)
+    'user_id': np.repeat(np.arange(lower, upper, 1), 1), # change to 9 if repeat for day 0-8
+    'day': np.tile(np.arange(1), (upper-lower)), # change to 9 if repeat for day 0 (consent) - day 8 (inclusive)
     'userName': 'userName',
     'cohort': 4,
     'treatment': 'T',
@@ -23,28 +19,30 @@ dict = {
 }
 df = pd.DataFrame(dict)
 
-index = 0
-for nextUserID in range(int(input_id_lower), int(input_id_upper)):
-    for day in range(9): # day 0 (consent) - day 8 (inclusive)
-        user_id_hashids = Hashids(salt=str(10 * nextUserID + day) + "user_id", min_length=16)
-        day_hashids = Hashids(salt=str(10 * nextUserID + day) + "day", min_length=10)
-        hashed_user_id = user_id_hashids.encrypt(nextUserID)
-        hashed_day = day_hashids.encrypt(day)
-
-        cohort = 4
-        if index % 8 == 0: # only assign treatment once (evey 8 days)
-            treatment = str(choices(treat_no, treat_prob)[0])
-
-        else:
-            treatment = df.at[index-1, 'treatment']
-        df.at[index, 'treatment'] = treatment
-        userName = 'userName'
-        df.at[index, 'link'] = "localhost:5000/shanghai/"+hashed_user_id+"/"+hashed_day+"/info"
-
-        requests.post("http://127.0.0.1:5000/userInsert/"+str(nextUserID)+"/"+
-            str(day)+"/"+str(userName)+"/"+ str(cohort) + "/" + str(treatment) +"/"+hashed_user_id+"/"+hashed_day)
-        index += 1
-    requests.post("http://127.0.0.1:5000/activityUpdate/"+str(nextUserID)+ "/1/0/0/0/0")
+index = 0 # keep track of the row index
+for user_id in range(lower, upper):
+    # hashing
+    user_id_hashids = Hashids(salt=str(10 * user_id + day) + "user_id", min_length=16)
+    day_hashids = Hashids(salt=str(10 * user_id + day) + "day", min_length=10)
+    hashed_user_id = user_id_hashids.encrypt(user_id)
+    hashed_day = day_hashids.encrypt(day)
+    # shanghai cohort
+    cohort = 4
+    # iterate through each treatment
+    treatment = treatments[user_id % 4]
+    df.at[index, 'treatment'] = treatment
+    # dummy username
+    userName = 'test' + str(user_id)
+    # create link
+    df.at[index, 'link'] = "localhost:5000/shanghai/"+hashed_user_id+"/"+hashed_day+"/info"
+    # update user table
+    requests.post("http://127.0.0.1:5000/userInsert/"+str(user_id)+"/"+
+        str(day)+"/"+str(userName)+"/"+ str(cohort) + "/" + str(treatment) +"/"+hashed_user_id+"/"+hashed_day)
+    # add a new row in the table
+    index += 1
+    # update to new day
+    requests.post("http://127.0.0.1:5000/activityUpdate/"+str(user_id)+ "/"+str(day)+"/0/0/0/0")
 
 print('\n')
 print(tabulate(df, headers='keys', tablefmt='psql'))
+print('\n(* Remember to run "init-db" in between tests for different days!)\n')
