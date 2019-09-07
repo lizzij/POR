a7d77a0b490b77b3ed7b7cbecea1d418daf4ddd3
eliza <elizazijinhuang@gmail.com>
2019-09-07 17:12:15 -0400

    to https

--- distribution/shanghai/insertPastUsersWechatID.py
+++ distribution/shanghai/insertPastUsersWechatID.py
@@ -4,7 +4,7 @@ import requests
 users = pd.read_csv("pastUsersWechatID.csv")
 
 def update_wechat_id(user):
-    requests.post("http://127.0.0.1:5000/userInsert/"+
+    requests.post("https://dailyeventinfo.com/userInsert/"+
         str(user['user_id'])+"/"+str(99)+"/"+str(user['wechat_id'])+"/"+str(user['cohort'])+
         "/treatment/hashed_user_id/hashed_day")
 
